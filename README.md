23f9c96e54f57a1d79cc67299348340ee37fb397
R4C00n <superc0605@gmail.com>
2024-04-19 00:37:49 +0700

    update main.py, README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Implementation of Hierarchical Attention Networks for Document Classification
+Zichao Yang1 , Diyi Yang1 , Chris Dyer1 , Xiaodong He2 , Alex Smola1 , Eduard Hovy1
+
+## Prerequisites
+### Install glove 
+```
+cd Hierachical-Attention-Network
+wget http://nlp.stanford.edu/data/glove.6B.200d.zip
+apt install unzip
+unzip "glove.42B.300d.zip"
+```
+## Dataset
+**Yelp 2013 Dataset**
+Can be found in https://zenodo.org/records/7555898
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch.optim as optim
 import pickle
 from Model import HierachicalAttentionNetwork as HAN
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     word_emmbed_dim = 200
@@ -13,22 +14,41 @@ if __name__ == '__main__':
     optimizer = optim.Adam(model.parameters())
     criterion = torch.nn.NLLLoss()
 
-    num_epochs = 1000
+    num_epochs = 10
 
     with open('Data/X.pickle','rb') as file:
         X = pickle.load(file)
     with open('Data/Y.pickle','rb') as file:
         Y = pickle.load(file)
     
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=42)
+    losses = []
     for i in range(num_epochs):
-        count = 0
+        running_loss = 0.0
         for input, target in zip(X_train, Y_train):
             output = model(input)
+
             loss = criterion(output,target)
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
-            count+=1
-    
+
+            running_loss += loss.item()
+
+        losses.append(running_loss)
+        print(f'epoch: {i}, loss: {running_loss}')
+
+    plt.plot(losses)
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.title('Loss')
+    plt.show()
+
+    correct = 0
+    with torch.no_grad():
+        for input,target in zip(X_test, Y_test):
+            Y_pred = model(input)
+            correct += (torch.argmax(Y_pred) == torch.argmax(target)).item()
+
+    print('Accuracy of the network on test set: %d %%' % (100 * correct / len(X_test)))
     
